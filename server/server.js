84d6c91c21810b0d6542409b4e4acbc14fee6038
InvizHer <112791490+InvizHer@users.noreply.github.com>
2024-11-01 14:23:38 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -29,9 +29,6 @@ app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
-import path from 'path';
-import { fileURLToPath } from 'url';
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
